bac3f8d6acd9cc7d95926ba6d771c6defd5249e8
Aleksey Shingarev <alekseytim@mail.ru>
2023-01-27 13:36:17 +0300

    feat:db

--- src/app.ts
+++ src/app.ts
@@ -5,17 +5,19 @@ import { StartCommand } from "./commands/start.command";
 import { IConfigService } from "./config/config.interface";
 import { ConfigService } from "./config/config.service";
 import { IBotContext } from "./context/context.interface";
+import { DatabaseService } from "./database/database.service";
 
 class Bot {
   bot: Telegraf<IBotContext>
   commands: Command[] = []
-  constructor(private readonly configService: IConfigService) {
+  constructor(private readonly configService: IConfigService, private readonly databaseService: DatabaseService) {
     this.bot = new Telegraf<IBotContext>(this.configService.get('TOKEN'))
     this.bot.use(
       new LocalSession({ database: 'sessions.json' }).middleware()
     )
   }
-  init() {
+  async init() {
+    await this.databaseService.init();
     this.commands = [new StartCommand(this.bot)]
     for (const command of this.commands) {
       command.handle()
@@ -24,5 +26,7 @@ class Bot {
   }
 }
 
-const bot = new Bot(new ConfigService());
+const database = new DatabaseService();
+
+const bot = new Bot(new ConfigService(), database);
 bot.init()
